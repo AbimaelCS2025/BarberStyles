4a3ae7487d1bb574d39b82f07fd3923c2f74d0fa
AbimaelCS2025 <acsboi2004@msn.com>
2025-05-21 00:12:42 -0300

    Update my-app.js

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -2,7 +2,7 @@
 document.addEventListener('deviceready', onDeviceReady.bind(this), false);
 
 //url para as apis
-var url = 'http://github.com/AbimaelCS2025/BarberStyles';
+var url = 'http://192.168.1.103/barbearia';
 var url_api = url + 'api/';
 var url_img = url + 'img/';
 
